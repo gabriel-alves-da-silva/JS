fbffda811b8601fd67646927462af7f2ded926c7
Gabriel Alves <gabrielalvesdasilva12452@gmail.com>
2023-09-09 09:13:27 -0300

    aula 11

---
+++ JavaScript - Aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if ( vel > 60) { //condição simples
+    console.log('Voce ultaprassou a velocidade maxima permitida  e foi multado!')
+}
+console.log('Dirija sempre com cinto de segurança')
